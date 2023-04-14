1ba32409a2ce87c7a7d8ca25c542a55974fe68f3
Alex Obringer <100440555+Obringerad@users.noreply.github.com>
2023-04-14 12:17:25 -0500

    Add first step and paly input html

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('hello world!');
+let rand = Math.floor(Math.random()*3);
+let computerSelection = '';
+let playerSelection='';
+document.getElementById('playerSelectionID').value=playerSelection;
+
+    if(rand == 0){
+        computerSelection = 'Rock';
+    } else if (rand == 1){
+        computerSelection = 'Paper';
+    } else{
+        computerSelection = 'Scissors';
+    }
+
+
+console.log(rand);
+console.log(computerSelection);
